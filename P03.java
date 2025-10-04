d0ee25a13dbcb45d6849425a5e73febf47840e65
Martí <mllaudhe7@alumnes.ub.edu>
2025-10-04 13:04:44 +0200

    Primer commit

---
+++ P03.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class P03{
+	public static void main (String[] args){
+		String nom;
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Cóm et dius?");
+		nom = scan.nextLine();
+		System.out.println("El teu nom és: " + nom);
+	}
+}
