dc79adbec04776e4250dddc92186049b399bb249
Martí <mllaudhe7@alumnes.ub.edu>
2025-10-04 13:37:19 +0200

    Primer commit P71

---
+++ P71.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class P71{
+	public static void main (String [] args){
+		Scanner entrada;
+		int x, y, z, suma;
+		String nom;
+		entrada = new Scanner (System.in);
+		System.out.println ("Introdueix el teu nom");
+		nom = entrada.next();
+		System.out.println(nom);
+	}
+} 
