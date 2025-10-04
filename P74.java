d6ad824f735539b3b34939e561bcd0a9e7319e47
Martí <mllaudhe7@alumnes.ub.edu>
2025-10-04 16:54:56 +0200

    Primer commit P74

---
+++ P74.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class P74{
+	public static void main (String[] args){
+		Scanner entrada;
+		Double b, z;
+		entrada = new Scanner (System.in);
+		System.out.println("x");
+		z = entrada.nextDouble();
+		b = Math.sqrt (z + 4);
+		System.out.println(b);  
+	}
+}
